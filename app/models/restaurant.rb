0dd31e5592b23760c24a3fd745f5f84dbd93ce2c
Ryan814 <rytg156@gmail.com>
2018-06-19 15:31:09 +0800

    add prev and next buttons

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,3 +1,11 @@
 class Restaurant < ApplicationRecord
   validates_presence_of :name
+
+  def prev
+    Restaurant.where("id < ?", id).last
+  end
+
+  def next
+    Restaurant.where("id > ?", id).first
+  end
 end
