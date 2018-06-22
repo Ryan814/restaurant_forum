01e608d455e49653b414963b5998e22908adb161
Ryan814 <rytg156@gmail.com>
2018-06-22 14:44:08 +0800

    link category and restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,7 @@
 class Restaurant < ApplicationRecord
   validates_presence_of :name
   mount_uploader :image, PhotoUploader
+  belongs_to :category
 
   def prev
     Restaurant.where("id < ?", id).last
