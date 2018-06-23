07ca0bacf10b626a188e9798fa5f5a36e41ceec9
Ryan814 <rytg156@gmail.com>
2018-06-23 21:18:54 +0800

    add delegate prevent error when category not exist

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,6 +2,7 @@ class Restaurant < ApplicationRecord
   validates_presence_of :name
   mount_uploader :image, PhotoUploader
   belongs_to :category
+  delegate :name, to: :category, prefix:true, allow_nil: true
 
   def prev
     Restaurant.where("id < ?", id).last
