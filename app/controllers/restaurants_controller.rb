8e0ed812b81c0d3176065add8edcc7ec1e6e9642
Ryan814 <rytg156@gmail.com>
2018-07-13 07:37:03 +0800

    setup routes for like/unlike & add like/unlike action on RestaurantController

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,4 +1,5 @@
 class RestaurantsController < ApplicationController
+    before_action :set_restaurant, only: [:show, :dashboard, :favorite, :unfavorite, :like, :unlike]
 
     def index
       @restaurants = Restaurant.page(params[:page]).per(9)
@@ -31,5 +32,22 @@ class RestaurantsController < ApplicationController
         favorite = Favorite.where(restaurant: @restaurant, user: current_user)
         favorite.destroy_all
         redirect_back(fallback_location: root_path)
+    end
+
+    def like
+        @restaurant.likes.create!(user: current_user)
+        redirect_back(fallback_location: root_path)
+    end
+
+    def unlike
+        like = Like.where(restaurant: @restaurant, user: current_user)
+        like.destroy_all
+        redirect_back(fallback_location: root_path)
+    end
+
+    private
+
+    def set_restaurant
+        @restaurant = Restaurant.find(params[:id])
     end
 end
