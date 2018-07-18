6944d673ff57eebcac820c4ba765e60528529168
Ryan814 <rytg156@gmail.com>
2018-07-19 06:59:20 +0800

    create followship model & setup self-referential relationships within users table through followships

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,12 +2,19 @@ class User < ApplicationRecord
   #若使用者已經發表評論，則不允許刪除帳號
   has_many :comments, dependent: :restrict_with_error
   has_many :restaurants, through: :comments
+  
   #「使用者能收藏很多餐廳」的多對多關聯
   has_many :favorites, dependent: :destroy
   has_many :favorited_restaurants, through: :favorites, source: :restaurant
+  
   #使用者能喜歡多間餐廳
   has_many :likes, dependent: :destroy
   has_many :liked_restaurants, through: :likes, source: :restaurant
+
+  #使用者能追蹤很多人也能被多人追蹤
+  has_many :followships, dependent: :destroy
+  has_many :followings, through: :followships
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
